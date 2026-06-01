c3de1c047216bad9d7f71906e4d290b0b239f416
wzk <cyber_shady@163.com>
2026-06-01 14:12:34 +0800

    fix: stub out auto_docstring to avoid transformers version compatibility issue

--- tower/models/neo_unify/modeling_qwen3.py
+++ tower/models/neo_unify/modeling_qwen3.py
@@ -22,7 +22,9 @@ from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutpu
 from transformers.modeling_rope_utils import ROPE_INIT_FUNCTIONS, dynamic_rope_update
 from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS, PreTrainedModel
 from transformers.processing_utils import Unpack
-from transformers.utils import TransformersKwargs, auto_docstring, can_return_tuple
+from transformers.utils import TransformersKwargs, can_return_tuple
+import functools as _functools
+auto_docstring = lambda f: f
 from transformers.utils.deprecation import deprecate_kwarg
 from transformers.utils.generic import check_model_inputs
 from transformers import Qwen3Config
--- tower/models/neo_unify/modeling_qwen3_moe.py
+++ tower/models/neo_unify/modeling_qwen3_moe.py
@@ -12,7 +12,8 @@ from transformers.modeling_layers import GradientCheckpointingLayer
 from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast
 from transformers.modeling_utils import PreTrainedModel
 from transformers.processing_utils import Unpack
-from transformers.utils import TransformersKwargs, auto_docstring, can_return_tuple
+from transformers.utils import TransformersKwargs, can_return_tuple
+auto_docstring = lambda f: f
 from transformers.utils.deprecation import deprecate_kwarg
 from transformers.utils.generic import check_model_inputs
 
